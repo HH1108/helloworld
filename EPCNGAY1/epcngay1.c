f36652b207c9ba25e01aa122776826e603a3516b
HH1108 <144683698+HH1108@users.noreply.github.com>
2023-09-11 18:59:32 +0700

    first commit
    test epc 1

---
+++ EPCNGAY1/epcngay1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,tong;
+	printf("Hello C programer");
+	printf("\nnhap a =");
+	scanf("%d",&a);
+	printf("nhap b =");
+	scanf("%d",&b);
+	tong = a+b;
+	printf("ket qua = %d", tong);
+	getch();
+}
